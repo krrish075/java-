540610bfe355edbbfd3b2c1b0984b2ce6fbb9888
Krrish <krrishgatade075@gmail.com>
2026-08-18 14:06:57 +0530

    learned recursion tracing

---
+++ DSA/Recursion/src/NumTrace.java
@@ -0,0 +1,12 @@
+public class NumTrace {
+    static void fun1(int n){
+        if(n>0){
+            System.out.println(n);
+            fun1(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        int x=3;
+        fun1(x);
+    }
+}
