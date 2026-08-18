614400d58420c5ad176ca12a51935d67242518a2
Krrish <krrishgatade075@gmail.com>
2026-08-18 08:04:21 +0530

    reverse string logic

---
+++ DSA/Recursion/src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void PrintNumber(int n){
+        if (n == 0){
+            return;
+        }
+        System.out.print(n);
+        PrintNumber(n - 1);
+    }
+    public static void main(String[] args) {
+        int n = 100;
+        PrintNumber(n);
+    }
+}
