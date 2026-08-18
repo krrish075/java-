04951b2d534aa7e7924d600c4e17f132e5ebd8b3
Krrish <krrishgatade075@gmail.com>
2026-08-18 14:35:28 +0530

    some upadtes mergin fun1 and fun2 also aka methods in java

--- DSA/Recursion/src/NumTrace.java
+++ DSA/Recursion/src/NumTrace.java
@@ -5,8 +5,19 @@ public class NumTrace {
             fun1(n-1);
         }
     }
+    static  void fun2(int n)
+    {
+        if(n>0){
+            fun2(n-1);
+            System.out.println(n);
+        }
+    }
     public static void main(String[] args) {
         int x=3;
         fun1(x);
+        System.out.println("function 1 print first and then perform logic and completely opposite for function 2 it perform all operation anf than print ");
+        System.out.println("Function 1 is over and function 2 begins from here");
+        fun2(x);
     }
+
 }
